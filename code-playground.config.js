421cc154bed476cdfd00276e73c977e8d810f16c
Olivier Bossel <olivier.bossel@gmail.com>
2019-01-17 10:29:54 +0100

    add distance detection method property

--- code-playground.config.js
+++ code-playground.config.js
@@ -14,6 +14,56 @@ module.exports = {
     port: 4000
   },
 
+  // demos
+  demos: {
+    circular: {
+      title: "Circular distance detection",
+      editors: {
+        html: {
+          language: "html",
+          data: `
+            <s-spring-snap snap-distance="50" max-translate="30" distance-detection-method="circular">
+              <button class="btn btn--primary btn--round">
+                <s-icon driver="fontawesome" icon="fab fa-amazon"></s-icon>
+              </button>
+            </s-spring-snap>
+          `
+        },
+        css: {
+          language: "scss",
+          data: `
+            @import 'node_modules/coffeekraken-sugar/index';
+            @import 'node_modules/coffeekraken-s-button-component/index';
+
+            @include s-setup(());
+            @include s-init();
+            @include s-classes();
+
+            @include s-button-classes();
+
+            .btn--round {
+              border-radius: 50%;
+              width: s-rem(50px); height: s-rem(50px);
+              text-align: center;
+              line-height: s-rem(50px);
+            }
+
+            body {
+              padding: s-space(bigger);
+            }
+          `
+        },
+        js: {
+          language: "js",
+          data: `
+            import SIconComponent from 'coffeekraken-s-icon-component'
+            import SSpringSnapComponent from './dist/index'
+          `
+        }
+      }
+    }
+  },
+
   // editors
   editors: {
     html: {
